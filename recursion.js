82bc843e7725e56f7987fec5e2bb7e889f5ee0b3
Metre-Ray <dgary@tut.by>
2018-10-27 23:04:54 +0300

    feat: add recursion.js

---
+++ recursion.js
@@ -0,0 +1,9 @@
+function recursion(tree, ans = [], i = 0) {
+    if (tree === undefined) return ;
+    if (ans[i] === undefined) ans.push([]);
+    ans[i].push(tree.value);
+    i +=1 ;
+    recursion(tree.left, ans, i);
+    recursion(tree.right, ans, i);
+    return ans;
+}
